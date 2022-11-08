cefa14b959abcba5428f302f4ee8ff9636af13d2
breno.ferrari <breno.ferrari@fiap.com.br>
2022-11-07 23:38:18 -0300

    Ajuste scss para mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,9 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      <head></head>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+      </head>
       <body>{children}</body>
     </html>
   )
